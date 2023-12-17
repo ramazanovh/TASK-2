92f11035d91378de3bed062ff7eb7c962688523a
ramazanovh <150849536+ramazanovh@users.noreply.github.com>
2023-12-17 21:24:27 +0400

    Add files via upload

---
+++ Myclass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task;
+public class MyClass<T>
+{
+    private T[] arr;
+
+    public MyClass()
+    {
+        arr = new T[0];
+    }
+
+    public void Add(T obj)
+    {
+        Array.Resize(ref arr, arr.Length + 1);
+        arr[arr.Length - 1] = obj;
+    }
+    public void ShowAll()
+    {
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    public int FirstIndex(T obj)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Equals(obj))
+            {
+                return i;
+            }
+        }
+        Console.WriteLine("The array does not have such object");
+        return -1;
+    }
+
+    //Muellim deyesen sinifde yazdigimizda bug var, hansi ki 1 + defe dalbadal silinmeli deyer verende sadece 1 ni silir.
+    // Bu asagidakini men ozum yazmisdim, burda bug yoxdur.
+    public void Remove(T obj)
+    {
+        T[] array = new T[arr.Length];
+        int count = 0;
+        int index = 0;
+        array = arr;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Equals(obj))
+            {
+                count++;
+            }
+            else
+            {
+                Array.Resize(ref arr, index + 1);
+                arr[index] = array[i];
+                index++;
+            }
+        }
+        if (count == 0)
+        {
+            Console.WriteLine("The array does not have such object");
+        }
+                //Task2
+
+        //    public void Remove(T obj) 
+        //    {
+        //        for (int i = 0; i <= arr.Length - 1; i++)
+        //        {
+        //            if (Equals(arr[i], obj))
+        //            {
+        //                arr[i] = default;
+        //                for (int j = i; j < arr.Length - 1; j++)
+        //                {
+        //                    arr[j] = arr[j + 1];
+        //                }
+        //                Array.Resize(ref arr, arr.Length - 1);
+        //            }
+        //        }
+        //    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//TASKin serti
+
+//"MyClass" class-ı yaradırsınız.Aşağıdakı metodları olmalıdır:
+//1.Add(T obj) - dərsdə yazmışıq
+//2. ShowAll() - dərsdə yazmışdıq
+//3. int FindİndexOf(T obj) - bir obyekt qəbul edir və ilk bu obyektə bərabər arrayin hər hansı bir dəyərini tapırsa onun index-sini geri qaytarır. Tapmasa -1
+//4. void Remove(T obj) - qəbul etdiyi parameter-ə uyğun object varsa remove etsin.
+
+MyClass<int> myClass = new();
+myClass.Add(1);
+myClass.Add(2);
+myClass.Add(2);
+myClass.Add(2);
+myClass.Add(3);
+myClass.Remove(8);
+myClass.ShowAll();
+Console.WriteLine(myClass.FirstIndex(2));
